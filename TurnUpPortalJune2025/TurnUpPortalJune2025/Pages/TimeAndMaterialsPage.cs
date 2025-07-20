6dd012096865d5d84b622f72565af136b35cb119
Rumaisa Shihab <rumaisashihab2102@gmail.com>
2025-07-20 20:42:20 +1200

    Edit and Delete time record modified

--- TurnUpPortalJune2025/TurnUpPortalJune2025/Pages/TimeAndMaterialsPage.cs
+++ TurnUpPortalJune2025/TurnUpPortalJune2025/Pages/TimeAndMaterialsPage.cs
@@ -41,24 +41,27 @@ namespace TurnUpPortalJune2025.Pages
             selectTime.Click();
 
             IWebElement enterCodeBox = WaitUntilVisible(By.Id("Code"));
-            enterCodeBox.SendKeys("TestRush");
+
+            enterCodeBox.SendKeys("TestCode");
 
             IWebElement enterDescription = WaitUntilVisible(By.Id("Description"));
+   
             enterDescription.SendKeys("Test Description");
 
 
             IWebElement enterPrice = WaitUntilVisible(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
-            enterPrice.SendKeys("12");
+  
+            enterPrice.SendKeys("14");
 
             IWebElement saveButton = WaitUntilClickable(By.Id("SaveButton"));
             saveButton.Click();
-            Thread.Sleep(3000);
+            Thread.Sleep(10000);
             IWebElement goLastPage = WaitUntilClickable(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
             goLastPage.Click();
             Thread.Sleep(3000);
             IWebElement lastRowRecord = WaitUntilVisible(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
             Thread.Sleep(3000);
-            Assert.That(lastRowRecord.Text == "TestRush", "Record not found");
+            Assert.That(lastRowRecord.Text == "TestCode", "Record not found");
            
 
         }
@@ -69,25 +72,29 @@ namespace TurnUpPortalJune2025.Pages
             IWebElement editLastRowRecord = WaitUntilClickable(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[5]/a[1]"));
             editLastRowRecord.Click();
             IWebElement editCodeBox = WaitUntilVisible(By.Id("Code"));
+            Console.WriteLine("Code Box found"); 
             editCodeBox.Clear();
             editCodeBox.SendKeys("NewTestRush");
 
 
             var editPrice = WaitUntilVisible(By.XPath("//input[contains(@class, 'k-formatted-value') and contains(@class, 'k-input')]"));
+            Console.WriteLine("Price Box found"); 
             editPrice.Clear();
-            editPrice.SendKeys("15");
+            Console.WriteLine("Price Box cleared");
+            //editPrice.SendKeys("15");
 
-            //IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
 
             // Set the value using JavaScript and trigger the 'change' event
-           // js.ExecuteScript("arguments[0].value = '13'; arguments[0].dispatchEvent(new Event('change'));", editPrice);
+            js.ExecuteScript("arguments[0].value = '13'; arguments[0].dispatchEvent(new Event('change'));", editPrice);
 
-
+            Console.WriteLine("Price entered");
 
             IWebElement editSaveButton = WaitUntilClickable(By.XPath("//*[@id=\"SaveButton\"]"));
             editSaveButton.Click();
-            Thread.Sleep(3000);
-
+            Thread.Sleep(10000);
+            Console.WriteLine("REcord edited");
+          
             IWebElement goLastPageEdit = WaitUntilClickable(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
             goLastPageEdit.Click();
 
@@ -102,13 +109,14 @@ namespace TurnUpPortalJune2025.Pages
             IWebElement deleteLastRowRecord = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[5]/a[2]"));
             deleteLastRowRecord.Click();
 
-
+            Console.WriteLine("Delete button clicked");
             IAlert alert = WaitForAlert();
             alert.Accept();
-
+            Console.WriteLine("Accept button clicked");
             IWebElement newLastRowRecord2 = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
             Thread.Sleep(2000);
-            Assert.That(newLastRowRecord2.Text != "NewTestRush", "Record not deleted");
+            Console.WriteLine("Last REcord checked");
+            Assert.That(newLastRowRecord2.Text == "NewTestRush", "Record deleted");
          
 
         }
